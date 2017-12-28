1d8ce532c1abfb43345044e4f0541ee1f97287be
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2017-12-28 14:50:50 -0200

    Remove unused import

--- app/src/main/java/com/udacity/bakingapp/stepdetails/VideoFragment.java
+++ app/src/main/java/com/udacity/bakingapp/stepdetails/VideoFragment.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
