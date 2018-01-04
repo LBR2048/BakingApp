ed21c49b396adc3cfc8c4c80692615ba5ab405ca
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2018-01-04 17:30:56 -0200

    Get recipes JSON file from server instead of using hardcoded String

---
+++ app/src/main/java/com/udacity/bakingapp/repository/RecipesEndpointInterface.java
@@ -0,0 +1,22 @@
+package com.udacity.bakingapp.repository;
+
+import com.udacity.bakingapp.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by leonardo.ardjomand on 04/01/2018.
+ */
+
+public interface RecipesEndpointInterface {
+
+    // Request method and URL specified in the annotation
+    // Callback for the parsed response is the last parameter
+
+    @GET("{recipes}")
+    Call<List<Recipe>> getRecipes(@Path("recipes") String recipes);
+}
