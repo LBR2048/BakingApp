ff8825fb550fda3f1e4b647d42cfc15681bde518
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2018-03-22 10:44:57 -0300

    Resume video from last position when rotating the device

--- app/src/main/java/com/udacity/bakingapp/stepdetails/VideoFragment.java
+++ app/src/main/java/com/udacity/bakingapp/stepdetails/VideoFragment.java
@@ -147,16 +147,6 @@ public class VideoFragment extends Fragment implements Player.EventListener {
                 new DefaultExtractorsFactory(), null, null);
     }
 
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
     //region ExoPlayer Event Listeners
     @Override
     public void onTimelineChanged(Timeline timeline, Object manifest) {
